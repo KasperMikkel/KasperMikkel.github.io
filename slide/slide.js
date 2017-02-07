4e93d8f4bc55c61126c50fa0a66bfd92406a0cc9
Mikkel <mikkelhauvgaard@gmail.com>
2017-02-07 14:41:21 +0100

    Update

---
+++ slide/slide.js
@@ -0,0 +1,16 @@
+var name = "";
+
+function init(n){
+	name = n;
+	console.log("gnfgj");
+}
+function start(){
+	alert("HHH");
+	console.log("gnfgj");
+
+	for (;;) {
+		setTimeout(function () {
+    	        alert("HHH");
+        	}, 5000);
+}
+}
