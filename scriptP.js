7060cda7275ddb01e9c482e2bae8c09b5fe7fa8c
Mikkel <mikkelhauvgaard@gmail.com>
2017-02-07 12:16:10 +0100

    Update

--- scriptP.js
+++ scriptP.js
@@ -10,39 +10,39 @@ var i = 1;
 function next(){
 	switch(i){
 	case 0:
-		document.getElementById('img1').src = 'img/Darw1.jpg';
-		document.getElementById('img2').src = 'img/ID1.jpg';
-		document.getElementById('img3').src = 'img/SkabD1.jpg';
+		document.getElementById('img1').src = '../img/Darw1.jpg';
+		document.getElementById('img2').src = '../img/ID1.jpg';
+		document.getElementById('img3').src = '../img/SkabD1.jpg';
 		break;
 	case 1:
-		document.getElementById('img1').src = 'img/Darw2.jpg';
-		document.getElementById('img2').src = 'img/ID2.jpg';
-		document.getElementById('img3').src = 'img/SkabD2.jpg';
+		document.getElementById('img1').src = '../img/Darw2.jpg';
+		document.getElementById('img2').src = '../img/ID2.jpg';
+		document.getElementById('img3').src = '../img/SkabD2.jpg';
 		break;
-	case 2:
-		document.getElementById('img1').src = 'img/Darw3.jpg';
-		document.getElementById('img2').src = 'img/ID3.jpg';
-		document.getElementById('img3').src = 'img/SkabD3.jpg';
+	case 2:../
+		document.getElementById('img1').src = '../img/Darw3.jpg';
+		document.getElementById('img2').src = '../img/ID3.jpg';
+		document.getElementById('img3').src = '../img/SkabD3.jpg';
 		break;
 	case 3:
-		document.getElementById('img1').src = 'img/Darw4.jpg';
-		document.getElementById('img2').src = 'img/ID4.jpg';
-		document.getElementById('img3').src = 'img/SkabD4.jpg';
+		document.getElementById('img1').src = '../img/Darw4.jpg';
+		document.getElementById('img2').src = '../img/ID4.jpg';
+		document.getElementById('img3').src = '../img/SkabD4.jpg';
 		break;
 	case 4:
-		document.getElementById('img1').src = 'img/Darw5.jpg';
-		document.getElementById('img2').src = 'img/ID5.jpg';
-		document.getElementById('img3').src = 'img/SkabD5.jpg';
+		document.getElementById('img1').src = '../img/Darw5.jpg';
+		document.getElementById('img2').src = '../img/ID5.jpg';
+		document.getElementById('img3').src = '../img/SkabD5.jpg';
 		break;
 	case 5:
-		document.getElementById('img1').src = 'img/Darw6.jpg';
-		document.getElementById('img2').src = 'img/ID6.jpg';
-		document.getElementById('img3').src = 'img/SkabD6.jpg';
+		document.getElementById('img1').src = '../img/Darw6.jpg';
+		document.getElementById('img2').src = '../img/ID6.jpg';
+		document.getElementById('img3').src = '../img/SkabD6.jpg';
 		break;
 	case 6:
-		document.getElementById('img1').src = 'img/Darw7.jpg';
-		document.getElementById('img2').src = 'img/ID7.jpg';
-		document.getElementById('img3').src = 'img/SkabD7.jpg';
+		document.getElementById('img1').src = '../img/Darw7.jpg';
+		document.getElementById('img2').src = '../img/ID7.jpg';
+		document.getElementById('img3').src = '../img/SkabD7.jpg';
 		i = -1;
 		break;
 	}
