118ae6036be3ac871ae841d185cb399379bbbad0
Mikkel <mikkelhauvgaard@gmail.com>
2017-02-07 08:02:10 +0100

    Update

--- scriptP.js
+++ scriptP.js
@@ -1,3 +1,26 @@
+var img1 = document.getElementById("img1");
+var img2 = document.getElementById("img2");
+var img3 = document.getElementById("img3");
+var i = 0;
+
 function next(){
-	window.location ='index.html';
+	switch(i){
+	case 0:
+		document.getElementById('img1').src = 'img/dino.jpg';
+		document.getElementById('img2').src = 'img/dino2.jpg';
+		document.getElementById('img3').src = 'img/cyano.jpg';
+		break;
+	case 1:
+		document.getElementById('img1').src = 'img/homo_ruf.jpg';
+		document.getElementById('img2').src = 'img/fossil1.jpg';
+		document.getElementById('img3').src = 'img/primate.jpg';
+		break;
+	case 2:
+		document.getElementById('img1').src = 'img/dino2.jpg';
+		document.getElementById('img2').src = 'img/dino.jpg';
+		document.getElementById('img3').src = 'img/primate2.jpg';
+		i = -1;
+		break;
+	}
+	i++;
 }
